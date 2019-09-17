33511ae0e5d0e846e539f5ad04bfb5f4c5e65d7f
Fred Scott <fscott@southeast.edu>
2019-09-17 09:49:17 -0500

    Initial commit

---
+++ database.php
@@ -0,0 +1,26 @@
+<?php
+    $dsn = 'mysql:host=localhost;dbname=renameMeToYourGroupName';
+    $username = 'root';
+    $password = '';
+
+    try {
+        $db= new PDO($dsn, $username, $password);
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        //Displays the exception and keeps on rolling, uncomment the exit if you want it to halt instead
+        //exit();
+    }
+    
+    function select_all()
+    {
+        global $db;
+ 
+      $query = 'SELECT * FROM yourtablename';
+      $statement = $db->prepare($query);
+      $statement->execute();
+      $results =  $statement->fetchAll();
+      return $results;
+    }
+
+?>
