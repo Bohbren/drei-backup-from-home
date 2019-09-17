37f2b1b77e8ade86ba3779a02304095d61775adc
dt420622 <dt420622@LR00909.southeast.edu>
2019-09-17 10:25:11 -0500

    Some validation

--- confirmation.php
+++ confirmation.php
@@ -1,11 +1,45 @@
 <?php
-$category_id = filter_input(INPUT_POST, 'category_id', FILTER_VALIDATE_INT);
-$code = filter_input(INPUT_POST, 'code');
-$name = filter_input(INPUT_POST, 'name');
-$price = filter_input(INPUT_POST, 'price', FILTER_VALIDATE_FLOAT);
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$firstName = filter_input(INPUT_POST, 'fName');
+$lastName = filter_input(INPUT_POST, 'lName');
+$userName = filter_input(INPUT_POST, 'uName');
+$eMail = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+
+// Validate inputs
+if ($firstName == null){
+    $error = "Enter your First Name";
+}
+if($lastName == null){
+    $error = "Enter your Last Name";
+}
+if($userName == null){
+    $error = "Enter your User Name";
+}
+if($eMail == null){
+    $error = "Enter your Email";
+}
+
+//        || $category_id == false ||
+//        $code == null || $name == null || $price == null || $price == false) {
+//    $error = "Invalid product data. Check all fields and try again.";
+//    include('error.php');
+//} else {
+//    require_once('database.php');
+//
+//    // Add the product to the database  
+//    $query = 'INSERT INTO products
+//                 (categoryID, productCode, productName, listPrice)
+//              VALUES
+//                 (:category_id, :code, :name, :price)';
+//    $statement = $db->prepare($query);
+//    $statement->bindValue(':category_id', $category_id);
+//    $statement->bindValue(':code', $code);
+//    $statement->bindValue(':name', $name);
+//    $statement->bindValue(':price', $price);
+//    $statement->execute();
+//    $statement->closeCursor();
+//
+//    // Display the Product List page
+//    include('index.php');
+
+?>
 
